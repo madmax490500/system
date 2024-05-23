02224ae1070e9357366589e91b09dd5fb1ad1d1c
jylee <jylee@gamepub.co.kr>
2024-05-23 16:44:41 +0900

    웹페이지수정및gunicorn웹서버설정

--- rss_feed.py
+++ rss_feed.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template_string
-from threading import Timer
+from threading import Timer, Lock
 import feedparser
 from datetime import datetime
 
@@ -19,30 +19,38 @@ feed_urls = [
     # 다른 RSS 피드 URL을 여기에 추가할 수 있습니다.
 ]
 
+# Mutex Lock 생성
+update_lock = Lock()
+
 def update_feeds():
     global latest_titles
 
     new_entries = []
 
-    for url in feed_urls:
-        feed = feedparser.parse(url)
-        channel_title = feed.feed.get('title', '알 수 없는 채널')
+    # Lock을 획득하여 다른 스레드가 update_feeds 함수를 호출하지 못하도록 함
+    with update_lock:
+        for url in feed_urls:
+            feed = feedparser.parse(url)
+            channel_title = feed.feed.get('title', '알 수 없는 채널')
 
-        entries = [(entry.title, entry.link) for entry in feed.entries[:10]]
-        updated_entries = [(title, link) for title, link in entries if (title, link) not in latest_titles.get(url, set())]
+            entries = [(entry.title, entry.link) for entry in feed.entries[:10]]
+            updated_entries = [(title, link) for title, link in entries if (title, link) not in latest_titles.get(url, set())]
 
-        if updated_entries:
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            for title, link in updated_entries:
-                message = f"{current_time} - '{channel_title}': <a href=\"{link}\" target=\"_blank\">{title}</a><br>"
-                new_entries.append(message)
-            latest_titles[url] = set(entries)
+            if updated_entries:
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                for title, link in updated_entries:
+                    message = f"{current_time} - '{channel_title}': <a href=\"{link}\" target=\"_blank\">{title}</a><br>"
+                    new_entries.append(message)
+                latest_titles[url] = set(entries)
 
-    if new_entries:
-        global feed_messages
-        feed_messages.extend(new_entries)
+        if new_entries:
+            global feed_messages
+            feed_messages.extend(new_entries)
 
-    Timer(UPDATE_INTERVAL, update_feeds).start()
+    # Timer 객체가 실행 중이 아닌 경우에만 새로운 Timer 객체 생성
+    if not hasattr(update_feeds, "timer") or not update_feeds.timer.is_alive():
+        update_feeds.timer = Timer(UPDATE_INTERVAL, update_feeds)
+        update_feeds.timer.start()
 
 # 초기 피드 메시지 리스트
 feed_messages = []
@@ -65,7 +73,7 @@ def home():
           // 페이지를 1분마다 새로고침하는 함수
           setTimeout(function(){
              window.location.reload(1);
-          }, 10000);  // 60000 밀리초 = 1분
+          }, 60000);  // 60000 밀리초 = 1분
         </script>
         <style>
           body {
@@ -82,7 +90,7 @@ def home():
       </head>
       <body>
         <div class="container">
-          <h3 class="my-4">RSS Feed</h3>
+          <h3 class="my-4">RSS feed checker</h3>
           <div id="feed-container" class="list-group">
             {% for message in feed_messages %}
               <div class="feed-message list-group-item">{{ message|safe }}</div>
@@ -94,4 +102,6 @@ def home():
     """, feed_messages=feed_messages)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
+
