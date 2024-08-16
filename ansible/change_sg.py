f1efa0bba5c35578080d2d139f27c759418e8250
madmax <2523@naver.com>
2024-08-16 17:09:08 +0900

    멀티플인스턴스적용 포문

--- ansible/change_sg.py
+++ ansible/change_sg.py
@@ -2,18 +2,19 @@ import boto3
 
 ec2 = boto3.client('ec2', region_name='ap-northeast-2')
 
-instance_id = 'i-09bdbb5fcd607d549'
-new_sg_id = 'sg-73c3411b'
+instance_ids = ['i-123']  # Add more instance ID ['i-123', 'i-456']
+new_sg_id = 'sg-123'
 
-# 현재시큐리티그룹확인
-response = ec2.describe_instances(InstanceIds=[instance_id])
-current_sg_ids = [sg['GroupId'] for sg in response['Reservations'][0]['Instances'][0]['SecurityGroups']]
+for instance_id in instance_ids:
+    # 현재 시큐리티 그룹 확인
+    response = ec2.describe_instances(InstanceIds=[instance_id])
+    current_sg_ids = [sg['GroupId'] for sg in response['Reservations'][0]['Instances'][0]['SecurityGroups']]
 
-# 신규시큐리티그룹추가 같은 VPC인지 확인할것
-if new_sg_id not in current_sg_ids:
+    # 신규 시큐리티 그룹 추가, 같은 VPC인지 확인할 것
+    if new_sg_id not in current_sg_ids:
         current_sg_ids.append(new_sg_id)
 
         # Modify the instance to update the security groups
         ec2.modify_instance_attribute(InstanceId=instance_id, Groups=current_sg_ids)
 
-        print("Security groups updated successfully.")
+        print(f"Security groups updated successfully for instance {instance_id}.")
